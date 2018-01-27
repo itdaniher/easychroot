85506a75f877c10c15234a787e4b33fe53d66ff2
itdaniher <itdaniher@gmail.com>
2018-01-27 18:21:37 +0000

    non-semantic modifications to pychroot

--- pychroot.py
+++ pychroot.py
@@ -1,59 +1,30 @@
-__PYCHROOT_LICENSE__ = """
-Copyright (c) 2018, Ian Daniher <@itdaniher>
-Copyright (c) 2014-2017, pychroot developers
-All rights reserved.
+# Copyright (c) 2018, Ian Daniher <@itdaniher>
+# Copyright (c) 2014-2017, pychroot developers
+# BSD 3-Clause License
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-   this list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-3. Neither the name of pychroot nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-"""
-
+import os
 import errno
 import logging
 import operator
-import os
-from functools import reduce 
+from functools import reduce
+from tinyoil import touch, mount, MS_BIND, MS_REC, MS_REMOUNT, MS_RDONLY, SplitExec, simple_unshare
 
-from tinyoil import touch
-from tinyoil import mount, MS_BIND, MS_REC, MS_REMOUNT, MS_RDONLY
-from tinyoil import SplitExec
-from tinyoil import simple_unshare
+ChrootError = ChrootMountError = Exception
 
-ChrootError=ChrootMountError=Exception
 
 def dictbool(dct, key):
-    """Check if a key exists and is True in a dictionary.
+    '''Check if a key exists and is True in a dictionary.
 
     :param dct: The dictionary to check.
     :type dct: dict
     :param key: The key to check
     :type key: any
-    """
+    '''
     return key in dct and isinstance(dct[key], bool) and dct[key]
 
 
 def getlogger(log, name):
-    """Gets a logger given a logger and a package.
+    '''Gets a logger given a logger and a package.
 
     Will return the given logger if the name is not generated from
     the current package, otherwise generate a logger based on __name__.
@@ -62,15 +33,12 @@ def getlogger(log, name):
     :type log: logging.Logger
     :param name: The __name__ of the caller.
     :type name: str
-    """
-    return (
-        log if isinstance(log, logging.Logger)
-        and not log.name.startswith(name.partition('.')[0])
-        else logging.getLogger(name))
+    '''
+    return log if isinstance(log, logging.Logger) and not log.name.startswith(name.partition('.')[0]) else logging.getLogger(name)
 
 
 def bind(src, dest, chroot, create=False, log=None, readonly=False, recursive=False, **_kwargs):
-    """Set up a bind mount.
+    '''Set up a bind mount.
 
     :param src: The source location to mount.
     :type src: str
@@ -86,12 +54,11 @@ def bind(src, dest, chroot, create=False, log=None, readonly=False, recursive=Fa
     :type readonly: bool
     :param recursive: Whether to use a recursive bind mount.
     :type recursive: bool
-    """
+    '''
     log = getlogger(log, __name__)
-    fstypes = ('proc', 'sysfs', 'tmpfs')
+    fstypes = 'proc', 'sysfs', 'tmpfs'
     mount_flags = []
     mount_options = []
-
     if src not in fstypes:
         src = os.path.normpath(src)
     if os.path.islink(dest):
@@ -100,7 +67,6 @@ def bind(src, dest, chroot, create=False, log=None, readonly=False, recursive=Fa
             create = True
     else:
         dest = os.path.normpath(dest)
-
     if create:
         try:
             if not os.path.isdir(src) and src not in fstypes:
@@ -110,19 +76,18 @@ def bind(src, dest, chroot, create=False, log=None, readonly=False, recursive=Fa
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise
-
         if not os.path.isdir(src) and src not in fstypes:
             try:
                 touch(dest)
             except OSError as e:
-                raise ChrootMountError(
-                    "cannot bind mount to '{}'".format(dest), getattr(e, 'errno', None))
-
+                raise ChrootMountError("cannot bind mount to '{}'".format(
+                    dest), getattr(e, 'errno', None))
     if not os.path.exists(src) and src not in fstypes:
-        raise ChrootMountError("cannot bind mount from '{}'".format(src), errno.ENOENT)
+        raise ChrootMountError(
+            "cannot bind mount from '{}'".format(src), errno.ENOENT)
     elif not os.path.exists(dest):
-        raise ChrootMountError("cannot bind mount to '{}'".format(dest), errno.ENOENT)
-
+        raise ChrootMountError(
+            "cannot bind mount to '{}'".format(dest), errno.ENOENT)
     if src in fstypes:
         fstype = src
         log.debug("  mounting '{}' filesystem on '{}'".format(src, dest))
@@ -132,22 +97,20 @@ def bind(src, dest, chroot, create=False, log=None, readonly=False, recursive=Fa
         if recursive:
             mount_flags.append(MS_REC)
         log.debug("  bind mounting '{}' on '{}'".format(src, dest))
-
     try:
-        mount(source=src, target=dest, fstype=fstype,
-              flags=reduce(operator.or_, mount_flags, 0),
-              data=','.join(mount_options))
+        mount(source=src, target=dest, fstype=fstype, flags=reduce(
+            operator.or_, mount_flags, 0), data=','.join(mount_options))
         if readonly:
             mount_flags.extend([MS_REMOUNT, MS_RDONLY])
-            mount(source=src, target=dest, fstype=fstype,
-                  flags=reduce(operator.or_, mount_flags, 0),
-                  data=','.join(mount_options))
+            mount(source=src, target=dest, fstype=fstype, flags=reduce(
+                operator.or_, mount_flags, 0), data=','.join(mount_options))
     except OSError as e:
         raise ChrootMountError(
             'Failed mounting: mount -t {} {} {}'.format(fstype, src, dest), e.errno)
-class Chroot(SplitExec):
 
-    """Context manager that provides chroot support.
+
+class Chroot(SplitExec):
+    '''Context manager that provides chroot support.
 
     This is done by forking, doing some magic on the stack so the contents are
     not executed in the parent, and executing the context in the forked child.
@@ -164,15 +127,9 @@ class Chroot(SplitExec):
     hostname. In order to set the chroot domain name as well specify an
         FQDN instead of a singular hostname.
     :type hostname: str
-    """
-
-    default_mounts = {
-        '/dev': {'recursive': True},
-        'proc:/proc': {},
-        'sysfs:/sys': {},
-        'tmpfs:/dev/shm': {},
-        '/etc/resolv.conf': {},
-    }
+    '''
+    default_mounts = {'/dev': {'recursive': True}, 'proc:/proc': {},
+                      'sysfs:/sys': {}, 'tmpfs:/dev/shm': {}, '/etc/resolv.conf': {}}
 
     def __init__(self, path, log=None, mountpoints=None, hostname=None, skip_chdir=False):
         super(Chroot, self).__init__()
@@ -182,33 +139,28 @@ class Chroot(SplitExec):
         self.skip_chdir = skip_chdir
         self.mountpoints = self.default_mounts.copy()
         self.mountpoints.update(mountpoints if mountpoints else {})
-
         if not os.path.isdir(self.path):
             raise ChrootError(
                 "cannot change root directory to '{}'".format(path), errno.ENOTDIR)
-
-        # flag mount points that require creation and removal
         for k, source, chrmount, opts in self.mounts:
             src = source
-            # expand mountpoints that are environment variables
             if source.startswith('$'):
                 src = os.getenv(source[1:], source)
                 if src == source:
                     if 'optional' in opts:
                         self.log.debug(
-                            'Skipping optional and nonexistent mountpoint '
-                            'due to undefined host environment variable: %s', source)
+                            'Skipping optional and nonexistent mountpoint due to undefined host environment variable: %s', source)
                         del self.mountpoints[k]
                         continue
                     else:
                         raise ChrootMountError(
                             'cannot mount undefined environment variable: {}'.format(source))
-                self.log.debug('Expanding mountpoint "%s" to "%s"', source, src)
+                self.log.debug(
+                    'Expanding mountpoint "%s" to "%s"', source, src)
                 self.mountpoints[src] = opts
                 del self.mountpoints[k]
                 if '$' in chrmount:
                     chrmount = os.path.join(self.path, src.lstrip('/'))
-
             if 'optional' not in opts and not os.path.exists(chrmount):
                 self.mountpoints[k]['create'] = True
 
@@ -224,14 +176,7 @@ class Chroot(SplitExec):
     def _child_setup(self):
         kwargs = {}
         if os.getuid() != 0:
-            # Enable a user namespace if we're not root. Note that this also
-            # requires a network namespace in order to mount sysfs and use
-            # network devices; however, we currently only provide a basic
-            # loopback interface if iproute2 is installed in the chroot so
-            # regular connections out of the namespaced environment won't work
-            # by default.
             kwargs.update({'user': True, 'net': True})
-
         simple_unshare(pid=True, hostname=self.hostname, **kwargs)
         self._mount()
         os.chroot(self.path)
@@ -239,30 +184,31 @@ class Chroot(SplitExec):
             os.chdir('/')
 
     def _cleanup(self):
-        # remove mount points that were dynamically created
         for _, _, chrmount, opts in self.mounts:
             if 'create' not in opts:
                 continue
-            self.log.debug('Removing dynamically created mountpoint: %s', chrmount)
+            self.log.debug(
+                'Removing dynamically created mountpoint: %s', chrmount)
             try:
                 if not os.path.isdir(chrmount):
                     os.remove(chrmount)
                     chrmount = os.path.dirname(chrmount)
                 os.removedirs(chrmount)
-            # don't fail if leaf directories aren't empty when trying to remove them
             except OSError:
                 pass
             except Exception as e:
-                raise ChrootMountError(
-                    "failed to remove chroot mount point '{}'".format(chrmount), getattr(e, 'errno', None))
+                raise ChrootMountError("failed to remove chroot mount point '{}'".format(
+                    chrmount), getattr(e, 'errno', None))
 
     def _mount(self):
-        """Do the bind mounts for this chroot object.
+        '''Do the bind mounts for this chroot object.
 
         This _must_ be run after creating a new mount namespace.
-        """
+        '''
         for _, source, chrmount, opts in self.mounts:
             if dictbool(opts, 'optional') and not os.path.exists(source):
-                self.log.debug('Skipping optional and nonexistent mountpoint: %s', source)
+                self.log.debug(
+                    'Skipping optional and nonexistent mountpoint: %s', source)
                 continue
-            bind(src=source, dest=chrmount, chroot=self.path, log=self.log, **opts)
+            bind(src=source, dest=chrmount,
+                 chroot=self.path, log=self.log, **opts)
